71ae90e720625c0958379acfdefdd589d2d59dc1
ajisahraji <56240052+ajisahraji@users.noreply.github.com>
2019-10-21 10:14:05 +0700

    Add files via upload

---
+++ Tugas Praktikum 2.py
@@ -0,0 +1,10 @@
+a = int(input("Masukan bilangan A:"))
+b = int(input("Masukan bilangan B:"))
+c = int(input("Masukan bilangan C:"))
+
+if a>b and a>c :
+      print(a)
+elif b>a and b>c :
+      print (b)
+elif c>a and c>b   :    
+      print(c)
